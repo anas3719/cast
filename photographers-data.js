3b3d397203d72a92a44ebd83c5081bdaee6b69bd
Anas3719 <omaranas3719@gmail.com>
2026-08-30 21:55:40 +0300

    Add Osama photographer profile

--- photographers-data.js
+++ photographers-data.js
@@ -4,5 +4,11 @@ window.photographers = [
     name: "المصور محمود الشبراوي",
     folderUrl: "https://drive.google.com/drive/folders/1xStPZz7nUZFLB1y8qppys0u2_8R7a2kY",
     photoUrl: "https://drive.google.com/thumbnail?id=1EuLgomQvRnTDSpi22MscSaAFWbjoRcWj&sz=w1000",
+  },
+  {
+    id: "osama-photographer",
+    name: "أسامة",
+    folderUrl: "https://drive.google.com/drive/folders/1e4M9Derlr9diKrL_LDOZBXb3d-8I8daT?usp=sharing",
+    photoUrl: "assets/osama-photographer.png",
   }
 ];
